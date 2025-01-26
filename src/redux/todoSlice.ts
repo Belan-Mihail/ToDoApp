d475899f6b73d80493c0c0ffc076c3dd9704a2da
Bilan Mykhailo <bilanmo1488@gmail.com>
2025-01-26 21:11:46 +0100

    create editTodoreducer and add comments to code

--- src/redux/todoSlice.ts
+++ src/redux/todoSlice.ts
@@ -1,6 +1,7 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 import { v4 as uuidv4 } from 'uuid';
 
+// Todo interface
 interface Todo {
     id: string,
     text: string,
@@ -8,6 +9,7 @@ interface Todo {
     category: string
 }
 
+// TodoState interface
 interface TodoState {
     todos: Todo[] 
 }
@@ -18,14 +20,17 @@ const loadTodoFromLocalStorage = () => {
     return savedTodos ? JSON.parse(savedTodos) : []
 }
 
+// initial todoState
 const initialState:TodoState = {
     todos: loadTodoFromLocalStorage()
 }
 
+// todoSlice
 const todoSlice = createSlice({
     name: 'todos',
     initialState,
     reducers: {
+        // add new Task
         addTodo: (state, action: PayloadAction<{text: string, category: string }>) => {
             const newTodo = {
                 id: uuidv4(),
@@ -36,12 +41,30 @@ const todoSlice = createSlice({
             state.todos.push(newTodo)
             localStorage.setItem('todos', JSON.stringify(state.todos))
         },
+        // switch complete status
         toggleTodo: (state, action: PayloadAction<string>) => {
             const todo = state.todos.find((todo) => todo.id === action.payload)
             if (todo) {
                 todo.complete = !todo.complete
                 localStorage.setItem('todos', JSON.stringify(state.todos))
             }
+        },
+        // delete task
+        removeTodo: (state, action: PayloadAction<string>) => {
+            state.todos = state.todos.filter((todo) => todo.id !== action.payload)
+            localStorage.setItem('todos', JSON.stringify(state.todos))
+        },
+        // edit task
+        editTodo: (state, action: PayloadAction<{id: string, NewText: string, NewCategory: string}>) => {
+            const todo = state.todos.find((todo) => todo.id === action.payload.id)
+            if (todo) {
+                todo.text = action.payload.NewText
+                todo.category = action.payload.NewCategory
+                localStorage.setItem('todos', JSON.stringify(state.todos))
+            }
         }
     }
 })
+
+export const {addTodo, toggleTodo, removeTodo, editTodo} = todoSlice.actions
+export default todoSlice.reducer
