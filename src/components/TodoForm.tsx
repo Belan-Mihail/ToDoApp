348a3bfc23c60d7e68b8a0ea76e89676ff3a722b
Bilan Mykhailo <bilanmo1488@gmail.com>
2025-01-27 20:41:57 +0100

    add needly imports in TodoForm.tsx

--- src/components/TodoForm.tsx
+++ src/components/TodoForm.tsx
@@ -1,4 +1,6 @@
-import React from 'react'
+import React, {useState} from 'react'
+import { Dispatch } from '@reduxjs/toolkit'
+import { addTodo } from '../redux/todoSlice'
 
 const TodoForm = () => {
   return (
