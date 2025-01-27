b2a370944f81d3d6b5915fe8441e2591583c9c58
Bilan Mykhailo <bilanmo1488@gmail.com>
2025-01-27 21:43:18 +0100

    add needly import to TodoList component

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,4 +1,7 @@
 import React from 'react'
+import { UseSelector } from 'react-redux'
+import { RootState } from '../redux/store'
+import { selectFilteredTodos } from '../redux/todoSlice'
 
 const TodoList = () => {
   return (
