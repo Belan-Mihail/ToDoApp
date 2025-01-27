9d0333006825da8e40f388412563295028f9ae97
Bilan Mykhailo <bilanmo1488@gmail.com>
2025-01-27 20:44:43 +0100

    add needly state to TodoForm component

--- src/components/TodoForm.tsx
+++ src/components/TodoForm.tsx
@@ -1,8 +1,13 @@
 import React, {useState} from 'react'
 import { Dispatch } from '@reduxjs/toolkit'
 import { addTodo } from '../redux/todoSlice'
+import { useDispatch } from 'react-redux'
 
-const TodoForm = () => {
+const TodoForm:React.FC = () => {
+  const [task, setTask] = useState<string>('')
+  const [category, setCategory] = useState<'family' | 'work' | 'privat'>('family')
+  const dispatch = useDispatch()
+
   return (
     <div>TodoForm</div>
   )
