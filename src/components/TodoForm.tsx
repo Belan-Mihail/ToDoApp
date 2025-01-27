66593483609d89b91e4fa80cb7d74028be540d41
Bilan Mykhailo <bilanmo1488@gmail.com>
2025-01-27 20:54:23 +0100

    define handleSubmit function in TodoForm.tsx

--- src/components/TodoForm.tsx
+++ src/components/TodoForm.tsx
@@ -1,12 +1,19 @@
 import React, {useState} from 'react'
-import { Dispatch } from '@reduxjs/toolkit'
 import { addTodo } from '../redux/todoSlice'
 import { useDispatch } from 'react-redux'
 
 const TodoForm:React.FC = () => {
   const [task, setTask] = useState<string>('')
-  const [category, setCategory] = useState<'family' | 'work' | 'privat'>('family')
+  const [category, setCategory] = useState<'family' | 'work' | 'private'>('family')
   const dispatch = useDispatch()
+
+  const handleSubmit = (e:React.FormEvent) => {
+    e.preventDefault()
+    if (!task.trim()) {
+      dispatch(addTodo({text: task, category}))
+      setTask('')
+    }
+  }
 
   return (
     <div>TodoForm</div>
