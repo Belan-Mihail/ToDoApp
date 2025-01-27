083e1547f7c4d595925bfc608a828dc753f3cafa
Bilan Mykhailo <bilanmo1488@gmail.com>
2025-01-27 22:57:34 +0100

    add needly states and const dispatch to TodoItem

--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -1,5 +1,5 @@
-import React from 'react'
-import { UseDispatch, useSelector } from 'react-redux'
+import React, { useState } from 'react'
+import { useDispatch, UseDispatch, useSelector } from 'react-redux'
 import { RootState } from '../redux/store'
 import { toggleTodo, removeTodo, editTodo, Todo } from '../redux/todoSlice'
 
@@ -8,6 +8,11 @@ interface TodoItemProps {
 }
 
 const TodoItem:React.FC<TodoItemProps> = ({ task }) => {
+    const [newText, setNewText] = useState<string>(task.text)
+    const [newCategory, setNewCategory] = useState<string>(task.category)
+    const [editFormIsOpen, setEditFormIsOpen] = useState<boolean>(false)
+    const dispatch = useDispatch()
+
   return (
     <div>TodoItem</div>
   )
