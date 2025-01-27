7115c0d042d6f7f39afd710d4deea95e9d8d60b4
Bilan Mykhailo <bilanmo1488@gmail.com>
2025-01-27 22:12:30 +0100

    create TodoListProps interface at TodoList component

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -3,7 +3,14 @@ import { UseSelector } from 'react-redux'
 import { RootState } from '../redux/store'
 import { selectFilteredTodos } from '../redux/todoSlice'
 
+
+interface TodoListProps {
+  todos: Todo[]
+  filter: {category: "family" | "work" | "private" | "all"; completed: "all" | "completed" | "incompleted"}
+}
+
 const TodoList = () => {
+
   return (
     <div>TodoList</div>
   )
