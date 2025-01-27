9d5e80f399581b9cbfb486ecee4edda190e2f305
Bilan Mykhailo <bilanmo1488@gmail.com>
2025-01-27 23:14:58 +0100

    define handleSaveEdit at TodoItem component

--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -9,7 +9,7 @@ interface TodoItemProps {
 
 const TodoItem:React.FC<TodoItemProps> = ({ task }) => {
     const [newText, setNewText] = useState<string>(task.text)
-    const [newCategory, setNewCategory] = useState<string>(task.category)
+    const [newCategory, setNewCategory] = useState<"family" | "work" | "private">(task.category)
     const [editFormIsOpen, setEditFormIsOpen] = useState<boolean>(false)
     const dispatch = useDispatch()
 
@@ -23,6 +23,14 @@ const TodoItem:React.FC<TodoItemProps> = ({ task }) => {
 
     const handleEditButtonClick = () => {
         setEditFormIsOpen(true)
+    }
+
+    const handleSaveEdit = (e:React.FormEvent) => {
+        e.preventDefault()
+        if (newText !== task.text || newCategory !== task.category) {
+            dispatch(editTodo(task.id, newText, newCategory))
+        }
+        setEditFormIsOpen(false)
     }
 
   return (
