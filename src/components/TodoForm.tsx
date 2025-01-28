f2eacae00b26ca9e1a991671b42d5793374ff6b4
Bilan Mykhailo <bilanmo1488@gmail.com>
2025-01-28 23:09:47 +0100

    add styles in App.css to hide initial select element

--- src/components/TodoForm.tsx
+++ src/components/TodoForm.tsx
@@ -25,7 +25,7 @@ const TodoForm:React.FC = () => {
   return (
     <form onSubmit={handleSubmit} className='p-4 m-4 flex gap-2 border-[#f8f4e5] border-2 rounded bg-[#292626cc]'>
       <input type="text" value={task} onChange={(e) => setTask(e.target.value)} placeholder='Add new task...' className='p-2 border-[#f8f4e5] border-2 bg-[#c5e62fcc] rounded-xl text-[#020920cc]'/>
-      <select value={category} onChange={(e) => setCategory(e.target.value as 'family' | 'work' | 'private')} className='p-2'>
+      <select value={category} onChange={(e) => setCategory(e.target.value as 'family' | 'work' | 'private')} className='px-8 border-[#f8f4e5] border-2'>
         <option value="family">family</option>
         <option value="work">work</option>
         <option value="private">private</option>
