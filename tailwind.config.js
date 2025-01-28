50f2ea36e8104d86a99c60abdb5ea56ef875fcc1
Bilan Mykhailo <bilanmo1488@gmail.com>
2025-01-29 00:20:53 +0100

    clean up code

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,13 @@
 module.exports = {
-    content: [
-      "./index.html", 
-      "./src/**/*.{js,ts,jsx,tsx}",  
-    ],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+   
+  ],
     theme: {
       extend: {
         colors: {
-          primary_black: '#292626cc',    
-          secondary_black: '#242020cc',
-          borders_white: '#f8f4e5',    
-          background_yellow: '#c5e62fcc', 
-          input_text: '#020920cc', 
-          agree_button: '#3dd472cc',    
+          
         },
       },
     },
