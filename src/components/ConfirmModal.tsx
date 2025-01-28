bc0f541861089238a28f85cdeb9aaab094d1d8b6
Bilan Mykhailo <bilanmo1488@gmail.com>
2025-01-28 22:20:35 +0100

    create ConfirmModalProps in ConfirmModal component

--- src/components/ConfirmModal.tsx
+++ src/components/ConfirmModal.tsx
@@ -1,5 +1,11 @@
 import React from 'react'
 
+interface ConfirmModalProps {
+    onConfirm: () => void
+    onCancel: () => void
+    header: string
+}
+
 const ConfirmModal = () => {
   return (
     <div>ConfirmModal</div>
