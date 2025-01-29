40aec1225091c961607635e2064503c37b6465f0
Bilan Mykhailo <bilanmo1488@gmail.com>
2025-01-29 12:52:25 +0100

    add styles and edit TodoList JSX

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -11,11 +11,19 @@ interface TodoListProps {
 const TodoList:React.FC<TodoListProps> = ({todos, filter}) => {
   const filteredTask = filteredTodos(todos, filter)
   return (
-    <div className='m-4'>
-      {filteredTask.map((task) => (
-        <TodoItem key={task.id} task={task} />
-      ))}
+    <>
+    <div className='flex items-center justify-center text-center mt-2'>
+        <h3>Current tasks</h3>
     </div>
+    <div className='p-4 mx-4 mt-2 flex flex-col gap-2 border-[#f8f4e5] border-2 rounded bg-[#292626cc] items-center justify-around'>
+      {filteredTask.length > 0 ? (filteredTask.map((task) => (
+        <TodoItem key={task.id} task={task} />
+      ))) : (
+        <p>You don't have any tasks yet.</p>
+      )}
+    </div>
+    </>
+   
   )
 }
 
