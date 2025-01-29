e2f08a47b93fe295324ef3feda809a9d5e603aab
Bilan Mykhailo <bilanmo1488@gmail.com>
2025-01-29 21:59:18 +0100

    finish with editing ConfirmModal component elements style

--- src/components/ConfirmModal.tsx
+++ src/components/ConfirmModal.tsx
@@ -9,11 +9,11 @@ interface ConfirmModalProps {
 const ConfirmModal:React.FC<ConfirmModalProps> = ({header, onConfirm, onCancel}) => {
   return (
     <div>
-        <div className='flex flex-col gap-2 items-center'>
+        <div className='flex gap-2 items-center'>
             <h3 className='text-delete-button'>{header}</h3>
             <div className='flex gap-2'>
-                <button className='w-[8rem] border-primary-light border-2 rounded-2xl bg-delete-button' onClick={onConfirm}>Confirm</button>
-                <button className='w-[8rem] border-primary-light border-2 rounded-2xl bg-edit-button' onClick={onCancel}>Cancel</button>
+                <button className='border-primary-light border-2 rounded-2xl p-2 bg-confirm-button' onClick={onConfirm}>Yes</button>
+                <button className='border-primary-light border-2 rounded-2xl p-2 bg-delete-button' onClick={onCancel}>No</button>
             </div>
         </div>
     </div>
