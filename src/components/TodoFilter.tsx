ff35e67906303202e2ba98ae7570df781ab66cb7
Bilan Mykhailo <bilanmo1488@gmail.com>
2025-01-29 11:14:34 +0100

    add primary styles to Todo Filter component

--- src/components/TodoFilter.tsx
+++ src/components/TodoFilter.tsx
@@ -20,17 +20,18 @@ const TodoFilter:React.FC = () => {
 
 
   return (
-    <div className='flex gap-2'>
-      <select value={currentFilters.category} onChange={handleCategoryChange} className='p-2 m-2'>
-        <option value="all">all</option>
-        <option value="family">family</option>
-        <option value="work">work</option>
-        <option value="private">private</option>
+    <div className='p-4 m-4 flex gap-2 border-[#f8f4e5] border-2 rounded bg-[#292626cc] items-center justify-around'>
+      <h3>Filters</h3>
+      <select value={currentFilters.category} onChange={handleCategoryChange} className='px-8 border-[#f8f4e5] border-2 rounded-xl'>
+        <option value="all" className='bg-[#292626cc] text-left'>all</option>
+        <option value="family" className='bg-[#292626cc] text-left'>family</option>
+        <option value="work" className='bg-[#292626cc] text-left'>work</option>
+        <option value="private" className='bg-[#292626cc] text-left'>private</option>
       </select>
-      <select value={currentFilters.completed} onChange={handleCompletionChange} className='p-2 m-2'>
-        <option value="all">all</option>
-        <option value="completed">completed</option>
-        <option value="incompleted">incompleted</option>
+      <select value={currentFilters.completed} onChange={handleCompletionChange} className='px-8 border-[#f8f4e5] border-2 rounded-xl'>
+        <option value="all" className='bg-[#292626cc] text-left'>all</option>
+        <option value="completed" className='bg-[#292626cc] text-left'>completed</option>
+        <option value="incompleted" className='bg-[#292626cc] text-left'>incompleted</option>
       </select>
     </div>
   )
