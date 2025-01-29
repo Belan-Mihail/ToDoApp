e2f4dcd06fc0ca719797369c052d8014505b6e2e
Bilan Mykhailo <bilanmo1488@gmail.com>
2025-01-29 11:28:56 +0100

    edit styles of TodoFilter elements

--- src/components/TodoFilter.tsx
+++ src/components/TodoFilter.tsx
@@ -25,17 +25,24 @@ const TodoFilter:React.FC = () => {
         <h3>Filters</h3>
       </div>
         <div className='flex justify-around gap-4 px-4'>
-          <select value={currentFilters.category} onChange={handleCategoryChange} className='px-8 border-[#f8f4e5] border-2 rounded-xl w-[10rem]'>
+          <div className='flex flex-col gap-2 text-center'>
+            <h4 className='text-sm italic'>by category</h4>
+            <select value={currentFilters.category} onChange={handleCategoryChange} className='px-8 border-[#f8f4e5] border-2 rounded-xl w-[10rem]'>
         <option value="all" className='bg-[#292626cc] text-left'>all</option>
         <option value="family" className='bg-[#292626cc] text-left'>family</option>
         <option value="work" className='bg-[#292626cc] text-left'>work</option>
         <option value="private" className='bg-[#292626cc] text-left'>private</option>
       </select>
-      <select value={currentFilters.completed} onChange={handleCompletionChange} className='px-8 border-[#f8f4e5] border-2 rounded-xl w-[10rem]'>
+          </div>
+          <div className='flex flex-col gap-2 text-center items-center'>
+            <h4 className='text-sm italic'>by completion</h4>
+             <select value={currentFilters.completed} onChange={handleCompletionChange} className='px-8 border-[#f8f4e5] border-2 rounded-xl w-[10rem] text-center'>
         <option value="all" className='bg-[#292626cc] text-left'>all</option>
         <option value="completed" className='bg-[#292626cc] text-left'>completed</option>
         <option value="incompleted" className='bg-[#292626cc] text-left'>incompleted</option>
       </select>
+          </div>
+     
         </div>
       
     </div>
