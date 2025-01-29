4f3b7c64a6398d9735eb9df5cad29c2cdf442241
Bilan Mykhailo <bilanmo1488@gmail.com>
2025-01-29 12:43:55 +0100

    editTodoForm JSX. add p with task.length and max task kenght

--- src/components/TodoForm.tsx
+++ src/components/TodoForm.tsx
@@ -38,7 +38,10 @@ const TodoForm:React.FC = () => {
         <h3>Add new task</h3>
     </div>
     <form onSubmit={handleSubmit} className='p-4 mx-4 mt-2 flex flex-col gap-2 border-[#f8f4e5] border-2 rounded bg-[#292626cc]'>
-      <input type="text" value={task} onChange={(e) => setTask(e.target.value)} placeholder='Add new task...' className='p-2 border-[#f8f4e5] border-2 bg-[#c5e62fcc] rounded-xl text-[#020920cc]'/>
+      <input type="text" value={task} onChange={handleTaskChange} placeholder='Add new task...' className='p-2 border-[#f8f4e5] border-2 bg-[#c5e62fcc] rounded-xl text-[#020920cc]'/>
+      <div className="text-sm italic text-[#f8f4e5] mt-1 text-right">
+          {task.length} / {maxLength} characters
+        </div>
       <div className='flex justify-around mt-2 mx-4 gap-4'>
         <div className='flex  justify-around gap-4 items-center'>
           <h4 className='text-sm italic'>Select category:</h4>
