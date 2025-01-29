965f281b6518e7344a260c9fdb3d2551151cab69
Bilan Mykhailo <bilanmo1488@gmail.com>
2025-01-29 11:20:57 +0100

    edit JSX code of TodoFilter component to display header in center and select in one row

--- src/components/TodoFilter.tsx
+++ src/components/TodoFilter.tsx
@@ -20,19 +20,24 @@ const TodoFilter:React.FC = () => {
 
 
   return (
-    <div className='p-4 m-4 flex gap-2 border-[#f8f4e5] border-2 rounded bg-[#292626cc] items-center justify-around'>
-      <h3>Filters</h3>
-      <select value={currentFilters.category} onChange={handleCategoryChange} className='px-8 border-[#f8f4e5] border-2 rounded-xl'>
+    <div className='p-4 m-4 flex flex-col gap-2 border-[#f8f4e5] border-2 rounded bg-[#292626cc] items-center justify-around'>
+      <div className='flex items-center justify-center text-center'>
+        <h3>Filters</h3>
+      </div>
+        <div className='flex justify-around gap-4 px-4'>
+          <select value={currentFilters.category} onChange={handleCategoryChange} className='px-8 border-[#f8f4e5] border-2 rounded-xl w-[10rem]'>
         <option value="all" className='bg-[#292626cc] text-left'>all</option>
         <option value="family" className='bg-[#292626cc] text-left'>family</option>
         <option value="work" className='bg-[#292626cc] text-left'>work</option>
         <option value="private" className='bg-[#292626cc] text-left'>private</option>
       </select>
-      <select value={currentFilters.completed} onChange={handleCompletionChange} className='px-8 border-[#f8f4e5] border-2 rounded-xl'>
+      <select value={currentFilters.completed} onChange={handleCompletionChange} className='px-8 border-[#f8f4e5] border-2 rounded-xl w-[10rem]'>
         <option value="all" className='bg-[#292626cc] text-left'>all</option>
         <option value="completed" className='bg-[#292626cc] text-left'>completed</option>
         <option value="incompleted" className='bg-[#292626cc] text-left'>incompleted</option>
       </select>
+        </div>
+      
     </div>
   )
 }
